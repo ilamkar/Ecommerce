f14276805bb232a4915d916c9216471602da326c
ilamkar <illum_dae@yahoo.com>
2014-12-10 22:58:56 -0600

    created the layout of the page

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Online shop</title>
+<link rel="stylesheet" type="text/css" href="css/style.css" media="all">
+</head> 
+
+<body>
+<!--Main wrapper start-->
+<div class="main_wrapper">
+	
+	<!--Header start-->
+	<div class="header_wrapper"> <!--Header starts -->
+		<img id="logo" src="images/logo.png" />
+		<img id="banner" src="images/ad_banner.gif"/>
+	</div> 	
+	<!--Header ends-->
+
+
+	<div class="menubar"> 
+		menubar
+	</div>
+
+	<!--Content wrapper starts-->
+	<div class="content_wrapper">
+		<div class="sidebar"> 
+			side bar
+
+		</div>
+	
+		<div class="content_area"> 
+			content area
+		</div>
+	</div> <!--Content wrapper ends-->
+
+	
+	<div class="footer"> 
+		footer
+	</div>
+
+</div> <!--main wrapper ends -->
+
+</body>
+</html>
