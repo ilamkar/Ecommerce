a7b86463013bdfbec7d193b88eade15b4ed054bf
ilamkar <illum_dae@yahoo.com>
2015-01-08 11:31:40 -0600

    get menus from the database dynamically

--- functions/functions.php
+++ functions/functions.php
@@ -6,7 +6,7 @@
 	// getting the categories
 	function getCategories(){
 		global $connect;
-		$run_categories=mysqli_query($connect,"SELECT * from categories");
+		$run_categories=mysqli_query($connect,"SELECT * FROM categories");
 		while($row_categories=mysqli_fetch_array($run_categories)){
 			
 			$category_id=$row_categories['category_id'];
@@ -19,13 +19,43 @@
 	// getting the brands
 	function getBrands(){
 		global $connect;
-		$run_brands=mysqli_query($connect,"SELECT * from brands");
+		$run_brands=mysqli_query($connect,"SELECT * FROM brands");
 		while($row_brands=mysqli_fetch_array($run_brands)){
 		
 			$brand_id = $row_brands['brand_id'];
 			$brand_title =$row_brands['brand_title'];
 
 			echo "<li><a href='#'>$brand_title</a></li>";
+		}
+	}
+
+	function getProducts(){
+		global $connect;
+		$get_products ="SELECT * FROM products ORDER BY RAND() LIMIT 0,6";
+
+		$run_products = mysqli_query($connect,$get_products);
+
+		while ($row_products =mysqli_fetch_array($run_products)) {
+			
+			$prod_id = $row_products['product_id'];
+			$prod_category = $row_products['product_category'];
+			$prod_brand = $row_products['product_brand'];
+			$prod_title = $row_products['product_title'];
+			$prod_price = $row_products['product_price'];
+			//$prod_desc = $row_products['product_description'];
+			$prod_image = $row_products['product_image'];
+			//$prod_keyword = $row_products['product_keyword'];
+
+			echo "<div id='single_product'>
+					<h3>$prod_title</h3>
+					<img src='admin/product_images/$prod_image' width='180' height='180' />
+					<p><b>$ $prod_price</b></p>
+
+					<a href='details.php?pro_id=$prod_id' style='float:left;'>Details</a>
+
+					<a href='index.php?pro_id=$prod_id'><button style='float:right;'>Add to Cart</button></a>
+
+			</div>";
 		}
 	}
 ?>
